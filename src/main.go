bd6d1c95a4a35a81c649a516539b0a5d64705efc
orifuji <orfj18@gmail.com>
2022-05-12 12:59:39 +0900

    feat(cli): とりあえず実行可能なコマンドに

--- src/main.go
+++ src/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var Command = &cobra.Command{
+	Use:   "command",
+	Short: "cmd",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("this is command")
+	},
+}
+
+func main() {
+	if err := Command.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
+		os.Exit(-1)
+	}
+}
